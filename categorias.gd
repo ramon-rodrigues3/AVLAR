04f5549e10eaa5823aca41cd5298b1950818ca40
Ramon Rodrigues <ramon333rodr@gmail.com>
2025-02-22 22:10:58 -0300

    evolução da cena main

--- categorias.gd
+++ categorias.gd
@@ -3,6 +3,13 @@ var categoriaAPI = Categoria.new()
 @onready var vbox = $VBoxContainer
 
 func _ready():
+	vbox.add_spacer(true)
+	
+	var add_new_categoria_button = Button.new()
+	add_new_categoria_button.text = "+"
+	add_new_categoria_button.custom_minimum_size = Vector2(200, 64)
+	vbox.add_child(add_new_categoria_button)
+	
 	for categoria in categoriaAPI.get_categoria_list():
 		var button = Button.new()
 		button.text = categoria
--- section.gd
+++ section.gd
@@ -4,12 +4,23 @@ var itemAPI = Item.new()
 @onready var vbox = $VBoxContainer
 
 func _ready():
+	vbox.add_spacer(true)
+	
+	var add_new_item_button = Button.new()
+	add_new_item_button.text = "+"
+	add_new_item_button.custom_minimum_size = Vector2(700, 200) 
+	add_new_item_button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+	add_new_item_button.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+	vbox.add_child(add_new_item_button)
+	
 	for item in itemAPI.get_item_list():
-		var panel = Panel.new()
-		panel.custom_minimum_size = Vector2(700, 200) 
-		panel.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
-		panel.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+		var button = Button.new()
+		button.custom_minimum_size = Vector2(700, 200) 
+		button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+		button.size_flags_vertical = Control.SIZE_SHRINK_CENTER
 		var label = Label.new()
 		label.text = item["nome"]
-		panel.add_child(label)
-		vbox.add_child(panel)
+		button.add_child(label)
+		vbox.add_child(button)
+	
+
